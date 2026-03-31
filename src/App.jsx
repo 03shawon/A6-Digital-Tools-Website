8fccf511828d978dfffbb41a76d5236b69a5f00c
03shawon <shawoncse5089@gmail.com>
2026-03-31 16:25:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className=' text-red-700 bg-amber-200'>hello world</h1>
+    <p className='font-bold text-amber-400 bg-blue-200'>Lorem ipsum dolor sit amet.</p>
+     <button className="btn btn-primary">Primary</button>
+
+    </>
+  )
+}
+
+export default App
