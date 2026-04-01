886f939a63029653060c58565ed63c4b1f4c28b2
03shawon <shawoncse5089@gmail.com>
2026-04-01 18:20:01 +0600

    some code update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,3 @@
-// import { useState } from 'react'
-
-// import './App.css'
-
-// function App() {
-
-//   return (
-//     <>
-//     <h1 className=' text-red-700 bg-amber-200'>hello world</h1>
-//     <p className='font-bold text-amber-400 bg-blue-200'>Lorem ipsum dolor sit amet.</p>
-//      <button className="btn btn-primary">Primary</button>
-
-//     </>
-//   )
-// }
-
-// export default App
 
 import { useState } from 'react'
 import Navbar from './components/Navbar'
